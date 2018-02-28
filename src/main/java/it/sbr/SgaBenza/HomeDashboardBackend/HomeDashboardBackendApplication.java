f24cb901d0a0fb49b222a7f2dbd62e80fc27f400
stefano <s.gallo@koor.it>
2018-02-28 17:53:49 +0100

    first commit

---
+++ src/main/java/it/sbr/SgaBenza/HomeDashboardBackend/HomeDashboardBackendApplication.java
@@ -0,0 +1,12 @@
+package it.sbr.SgaBenza.HomeDashboardBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeDashboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeDashboardBackendApplication.class, args);
+	}
+}
